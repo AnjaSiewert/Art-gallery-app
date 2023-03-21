d025f103663ba9a078475370ca0be5d3e93ec942
Anja Siewert <anja.siewert@live.de>
2023-03-21 16:53:17 +0100

    add links User story3

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
+import Layout from "../components/Layout/Layout";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
+
+  if (error) return "An error has occurred.";
+  if (isLoading) return "is loading";
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Layout />
+      <Component {...pageProps} pieces={data} />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,10 @@
-import ArtPieces from "../components/ArtPieces/ArtPieces";
-import useSWR from "swr";
 import Spotlight from "../components/Spotlight/Spotlight";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
-export default function HomePage() {
-  const { data, error, isLoading } = useSWR(
-    "https://example-apis.vercel.app/api/art",
-    fetcher
-  );
-
-  if (error) return "An error has occurred.";
-  if (isLoading) return "is loading";
-
+export default function SpotlightPage({ pieces }) {
   return (
     <div>
       <h1>Art pieces</h1>
-      <ArtPieces pieces={data} />
-      <Spotlight pieces={data} />
+      <Spotlight pieces={pieces} />
     </div>
   );
 }
